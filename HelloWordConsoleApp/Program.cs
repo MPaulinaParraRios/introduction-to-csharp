9ddcaff554584465033047562aa091a5983595d4
Paulina <mariaparra316818@correo.itm.edu.co>
2023-08-29 17:36:03 -0500

    Add my first console App

---
+++ HelloWordConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWordConsoleApp;
+
+public static class sayHello
+{
+    public static void Main(){
+    var person = new Person();
+    person.Name = "Jhon";
+    person.Age = 18;
+
+    string message = $"Hello Mr {person.Name} your age is {person.Age}";
+    Console.WriteLine(message);
+    Console.ReadLine();
+    }
+
+}
+
+
